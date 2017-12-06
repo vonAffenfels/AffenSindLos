afc919859843c710fa8a4ed8a7ef753e8bc2a177
Okan Aydogan <okan.aydogan@vonaffenfels.de>
2017-12-06 16:17:58 +0100

    animal speed

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 var GameStarted = false;
 //Strecke
-var trackL;
-var trackR;
 var myBackground;
 //Enemy
 var myAnimal = [];
 //Character
 var car;
-var carPosX = window.innerWidth/2;
+var carPosX = window.innerWidth / 2;
 var carX;
-var carPosY = window.innerHeight-250;
+var carPosY = window.innerHeight - 250;
 var carY;
 var newAnimals = true;
 //Animals
-var animalSprite = ['ape','bock','kingkong'];
-var animalSpeed = [3, 4, 5];
-var animalDMG = [100, 300, 500];
-var animalDirection =['left','right'];
+var animalSprite = [
+    'ape',
+    'bock',
+    'kingkong'
+];
+var animalSpeedY = 5;
+var animalSpeed = [
+    3,
+    4,
+    5
+];
+var animalDMG = [
+    100,
+    300,
+    500
+];
+var animalDirection = [
+    'left',
+    'right'
+];
 var speed;
 var animalx;
 //Controller
@@ -25,6 +39,8 @@ var breakButton;
 var breakDMG;
 //Score
 var myScore;
+var GameOverText;
+var GameEnd = false;
 
 function show_SoundMessage() {
     var soundfile = "audio/serengeti_brake.mp3";
@@ -38,23 +54,32 @@ function show_SoundMessage() {
 function startGame() {
 
     show_SoundMessage();
+    document.getElementById('restart').style = 'display:none';
+    GameEnd = false;
     //Strecke
     myBackground = new road(window.innerWidth, window.innerHeight, 0, -60, "./img/road/road-1.png");
     //Auto
     carX = carPosX;
     carY = carPosY;
-    car = new char(50, 90, carX, carY, "./img/char/car.png");
+    car = new char(window.innerWidth / 10, window.innerHeight / 8, carX, carY, "./img/char/car.png");
     //Button
-    clickField = new track(window.innerWidth, 75, "black", 0, window.innerHeight-75);
-    breakButton = new controller(72, 72, "./img/button-pause.png", window.innerWidth/2, window.innerHeight-75);
-    myScore = new score("30px", "LiquorstoreJazz", "white", 20, window.innerHeight-30, "text");
+    clickField = new track(window.innerWidth, 75, "black", 0, window.innerHeight - 75);
+    breakButton = new controller(72, 72, "./img/button-pause.png", window.innerWidth / 2, window.innerHeight - 75);
+    myScore = new score("30px", "LiquorstoreJazz", "white", 20, window.innerHeight - 30, "text");
+    GameOverText = new gameover("30px", "LiquorstoreJazz", "black", 20, 30, "text");
     myGameArea.start();
 
 }
 
+function restart() {
+
+    document.getElementById('restart').style = '';
+
+}
+
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
+    canvas: document.createElement("canvas"),
+    start: function () {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.context = this.canvas.getContext("2d");
@@ -75,10 +100,10 @@ var myGameArea = {
             clearMove();
         })
     },
-    clear : function() {
+    clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-    stop : function() {
+    stop: function () {
         clearInterval(this.interval);
     }
 };
@@ -90,14 +115,14 @@ function track(width, height, color, x, y) {
     this.speedY = 0;
     this.x = x;
     this.y = y;
-    this.update = function() {
+    this.update = function () {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     };
-    this.newPos = function() {
+    this.newPos = function () {
         this.x += this.speedX;
-        if(this.y >= 800){
+        if (this.y >= 800) {
             this.y = -145;
         }
         this.y += this.speedY;
@@ -112,12 +137,12 @@ function road(width, height, x, y, imgSrc) {
     this.speedY = 5;
     this.x = x;
     this.y = y;
-    this.update = function() {
+    this.update = function () {
         ctx = myGameArea.context;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         ctx.drawImage(this.image, this.x, (this.y - this.height), this.width, this.height);
     };
-    this.newPos = function() {
+    this.newPos = function () {
         this.y += this.speedY;
         if (this.y === this.height || this.y >= this.height) {
             this.y = 0;
@@ -134,17 +159,17 @@ function char(width, height, x, y, imgSrc) {
     this.speedX = 0.90;
     this.x = x;
     this.y = y;
-    this.update = function() {
+    this.update = function () {
         ctx = myGameArea.context;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     };
-    this.newPos = function() {
+    this.newPos = function () {
         this.x += this.speedX;
-        if(this.x >= carPosX+30+30){
+        if (this.x >= carPosX + 30 + 30) {
             this.speedX = -0.90;
             this.direction = "left";
         }
-        if(this.x <= carPosX-30){
+        if (this.x <= carPosX - 30) {
             this.speedX = 0.90;
             this.direction = "right";
         }
@@ -156,23 +181,23 @@ function animal(width, height, x, y, imgSrc, speed, DMG) {
     this.height = height;
     this.image = new Image();
     this.image.src = imgSrc;
-    this.speedY = 10;
+    this.speedY = 5;
     this.speedX = speed;
     this.points = DMG;
     this.status = true;
     this.x = x;
     this.y = y;
-    this.update = function() {
+    this.update = function () {
         ctx = myGameArea.context;
-        ctx.drawImage(this.image,this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     };
-    this.newPos = function() {
+    this.newPos = function () {
         this.y += this.speedY;
         this.x += this.speedX;
-        if(car.y <= (this.y+49) && car.y >= (this.y-91)) {
-            if(car.x <= (this.x+50) && car.x >= (this.x-50)){
+        if (car.y <= (this.y + 49) && car.y >= (this.y - 91)) {
+            if (car.x <= (this.x + 50) && car.x >= (this.x - 50)) {
                 //punkte runterzählen
-                if(this.status == true) {
+                if (this.status == true) {
                     myScore.scorePoints = myScore.scorePoints - this.points;
                     this.status = false
                 }
@@ -182,7 +207,7 @@ function animal(width, height, x, y, imgSrc, speed, DMG) {
 }
 
 function everyinterval(n) {
-    if((myGameArea.frameNo / n) % 1 == 0) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {
         return true;
     }
     return false;
@@ -198,84 +223,89 @@ function updateGameArea() {
     //Set new Objects
     car.update();
     //neue Tiere setzen
-    if(newAnimals == true) {
+    if (newAnimals == true) {
         myGameArea.frameNo += 1;
         if (myGameArea.frameNo == 1 || everyinterval(20)) {
             var pos = (Math.floor((Math.random() * 3) + 1) - 1);
             var direction = (Math.floor((Math.random() * 2) + 1) - 1);
-            if(direction == 0){
-                animalx = window.innerWidth-100;
+            if (direction == 0) {
+                animalx = window.innerWidth - 100;
                 speed = animalSpeed[pos] * (-1);
             }
-            if(direction == 1){
+            if (direction == 1) {
                 animalx = 50;
                 speed = animalSpeed[pos];
             }
-            myAnimal.push(new animal(50, 50, animalx, 0, "./img/animal/"+ animalDirection[direction] + "_" + animalSprite[pos] +".png", speed, animalDMG[pos]));
+
+            myAnimal.push(new animal(50, 50, animalx, 0, "./img/animal/" + animalDirection[direction] + "_" + animalSprite[pos] + ".png", speed, animalDMG[pos]));
         }
     }
     for (var i = 0; i < myAnimal.length; i++) {
-        if(myAnimal[i].status) {
+        if (myAnimal[i].status) {
             myAnimal[i].newPos();
             myAnimal[i].update();
         }
     }
     clickField.update();
     breakButton.update();
-    if(newAnimals == true) {
+    if (newAnimals == true) {
         if (everyinterval(60)) {
             myScore.scorePoints -= 1;
         }
     }
-    if(breakDMG == true){
+    if (breakDMG == true) {
         myScore.scorePoints -= 250;
         breakDMG = false;
     }
 
-    myScore.text="SCORE: " + myScore.scorePoints;
+    myScore.text = "SCORE: " + myScore.scorePoints;
     myScore.update();
-    if(myScore.scorePoints <= 0){
+    if (myScore.scorePoints <= 0) {
         myGameArea.clear();
         //GameOver.update();
         clickField.update();
-        var engine = document.getElementById('engine');
-        engine.pause();
-        myScore.text  = "Persönlicher High-Score: " + (Math.floor((myGameArea.frameNo / 1000)));
+        //GameOver Text + Click to Retry
+        GameOverText.text = "Game Over Mate Click to retry";
+        GameOverText.update();
+        GameEnd = true;
+        myScore.text = "Persönlicher High-Score: " + (Math.floor((myGameArea.frameNo / 100)));
         myScore.update();
         //Highscore-Liste
 
         //Retry Button
 
-        //Beenden Button
-
         myGameArea.stop();
+        restart();
 
     }
 }
 
-function clearMove(){
-    if(car.direction === "right"){
+function clearMove() {
+    if (car.direction === "right") {
         car.speedX = 5;
     }
-    if(car.direction === "left"){
+    if (car.direction === "left") {
         car.speedX = -5;
     }
     myBackground.speedY = 5;
-    for(var i = 0; i < myAnimal.length; i++){
-        myAnimal[i].speedY = 10;
+    for (var i = 0; i < myAnimal.length; i++) {
+        myAnimal[i].speedY = 5;
     }
+    animalSpeedY = 5;
     newAnimals = true;
 }
 
 function stopMove() {
     var breaksound = document.getElementById('soundbox');
-    myBackground.speedY = 0;
+    myBackground.speedY = 2;
     car.speedX = 0;
-    breaksound.play();
-    for(var i = 0; i < myAnimal.length; i++){
-        myAnimal[i].speedY = 0;
+    if (GameEnd === false) {
+        breaksound.play();
     }
-    newAnimals = false;
+    for (var i = 0; i < myAnimal.length; i++) {
+        myAnimal[i].speedY = 2;
+    }
+    animalSpeedY = 2;
     breakDMG = true;
 
 }
@@ -287,12 +317,13 @@ function controller(height, width, imgSrc, x, y) {
     this.image.src = imgSrc;
     this.x = x;
     this.y = y;
-    this.update = function() {
+    this.update = function () {
         ctx = myGameArea.context;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
 
 }
+
 function score(size, font, color, x, y, type) {
     this.type = type;
     this.size = size;
@@ -300,7 +331,7 @@ function score(size, font, color, x, y, type) {
     this.x = x;
     this.y = y;
     this.scorePoints = 3000;
-    this.update = function() {
+    this.update = function () {
         ctx = myGameArea.context;
         ctx.font = this.size + " " + this.font;
         ctx.fillStyle = color;
@@ -308,4 +339,18 @@ function score(size, font, color, x, y, type) {
     }
 }
 
+function gameover(size, font, color, x, y, type) {
+    this.type = type;
+    this.size = size;
+    this.font = font;
+    this.x = x;
+    this.y = y;
+    this.text = "GAMEOVER DU MOE"
+    this.update = function () {
+        ctx = myGameArea.context;
+        ctx.font = this.size + " " + this.font;
+        ctx.fillStyle = color;
+        ctx.fillText(this.text, this.x, this.y);
+    }
+}
 
